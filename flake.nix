25ba106b13b24ab26fd7a6329d77ca74b9beda4c
airflower <nebosra@proton.me>
2025-09-11 11:46:34 +0200

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+
+{
+  description = "CLI YouTube MPC-like client in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "youtube-mpc";
+    };
+}
