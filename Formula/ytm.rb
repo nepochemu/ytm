f25f4e8d1d30f526233cbfa6cba5058a360aaf19
airflower <nebosra@proton.me>
2025-09-14 11:29:05 +0200

    removed homebre-ytm repository, intgrated ytm.rb file

---
+++ Formula/ytm.rb
@@ -0,0 +1,21 @@
+class Ytm < Formula
+  desc "YouTube terminal music client"
+  homepage "https://github.com/nepochemu/ytm"
+  version "0.1.1"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/nepochemu/ytm/releases/download/v0.1.1/ytm-macos-arm64.tar.gz"
+      sha256 "<sha256_arm64>"
+    end
+
+    on_intel do
+      url "https://github.com/nepochemu/ytm/releases/download/v0.1.1/ytm-macos-x86_64.tar.gz"
+      sha256 "<sha256_x86_64>"
+    end
+  end
+
+  def install
+    bin.install "ytm"
+  end
+end
