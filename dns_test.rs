04d146a3037f0e3ed154c1d32d5aafea332e285f
airflower <nebosra@proton.me>
2025-09-18 15:27:53 +0200

    Clean up files

--- dns_test.rs
+++
@@ -1,4 +0,0 @@
-use std::net::ToSocketAddrs;
-fn main() {
-    println!("{:?}", "www.google.com:80".to_socket_addrs());
-}
--- reqwest_test.rs
+++
@@ -1,6 +0,0 @@
-use reqwest::blocking::get;
-
-fn main() {
-    let resp = get("https://www.google.com");
-    println!("{:?}", resp);
-}
